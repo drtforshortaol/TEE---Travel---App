7398b3d46690eebe60b570848585f89fd20e83c6
Glenn Takenaga DDS. MSc <drtforshort@aol.com>
2026-09-05 16:39:44 -0700

    Bump Quick Reference cache for clean Vault refactor

--- apps/travel-essentials/sw.js
+++ apps/travel-essentials/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tee-essentials-v3-4-77-live-vault-payload';
+const CACHE = 'tee-essentials-v3-4-78-clean-vault-refactor';
 const CACHE_PREFIX = 'tee-essentials-';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.json','../../traveler-help.css','../../traveler-help.js','../../protected-context.js','../../vault-session.js'];
 
