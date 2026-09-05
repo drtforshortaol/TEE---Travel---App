c9f0107fcd1c7b3d22ca5c9f1c30e0c5a19e9c49
Glenn Takenaga DDS. MSc <drtforshort@aol.com>
2026-09-05 15:44:55 -0700

    v3.4.71: refresh Quick Reference vault-session cache

--- apps/travel-essentials/sw.js
+++ apps/travel-essentials/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tee-essentials-v3-4-65-direct-contact-retrieval';
+const CACHE = 'tee-essentials-v3-4-71-same-tab-vault-continuity';
 const CACHE_PREFIX = 'tee-essentials-';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.json','../../traveler-help.css','../../traveler-help.js','../../protected-context.js','../../vault-session.js'];
 
